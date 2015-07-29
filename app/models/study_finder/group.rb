af592d50e240904d0729a8c95169310f6152eaea
blackjk3 <blackjk@gmail.com>
2015-07-29 08:46:02 -0500

    Adding validators to group and user models.  Fixes #8

--- app/models/study_finder/group.rb
+++ app/models/study_finder/group.rb
@@ -1,6 +1,8 @@
 class StudyFinder::Group < ActiveRecord::Base
   self.table_name = 'study_finder_groups'
 
+  validates :group_name, presence: true
+
   has_many :condition_groups
   has_many :conditions, through: :condition_groups
   has_many :subgroups
--- app/models/study_finder/user.rb
+++ app/models/study_finder/user.rb
@@ -1,6 +1,10 @@
 class StudyFinder::User < ActiveRecord::Base
   self.table_name = 'study_finder_users'
 
+  validates :first_name, :last_name, :email, :internet_id, presence: true
+  validates :email, :internet_id, uniqueness: true
+  validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+
   def full_name
     "#{self.first_name} #{self.last_name}".html_safe
   end
