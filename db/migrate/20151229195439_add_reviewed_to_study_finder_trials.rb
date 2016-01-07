8736c7f7edeb11010680cf9e82793bf990ea088a
blackjk3 <blackjk@gmail.com>
2016-01-07 12:52:00 -0600

    Removing default value on reviewed column since compressed oracle tables do not like that.

--- db/migrate/20151229195439_add_reviewed_to_study_finder_trials.rb
+++ db/migrate/20151229195439_add_reviewed_to_study_finder_trials.rb
@@ -1,5 +1,5 @@
 class AddReviewedToStudyFinderTrials < ActiveRecord::Migration
   def change
-    add_column :study_finder_trials, :reviewed, :boolean, default: false
+    add_column :study_finder_trials, :reviewed, :boolean
   end
 end
