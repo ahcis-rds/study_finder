dc56cb5ad63b1a719f2f649f016ae3ae02c6f3f1
Kong Yang <kyang026@gmail.com>
2016-11-28 14:08:52 -0600

    Added a captcha feature

---
+++ db/migrate/20161121182621_add_captcha_to_study_finder_trials.rb
@@ -0,0 +1,19 @@
+class AddCaptchaToStudyFinderTrials < ActiveRecord::Migration
+  def up
+	  if Rails.env == 'local'
+	    # We are using postgres locally, which supports a boolean type.
+	    default = 'FALSE'
+	    type = 'BOOLEAN'
+	  else
+	    # This is an oracle thing.  Oracle doesn't support boolean types. If you are not using oracle, please remove this sillyness and just use visible = true.
+	    default = '0'
+	    type = 'NUMBER(1,0)'
+	  end
+
+	  execute "ALTER TABLE study_finder_trials ADD captcha #{type} NOT NULL DEFAULT #{default};"
+	end
+
+	def down
+	  execute "ALTER TABLE study_finder_trials DROP COLUMN captcha;"
+	end
+end
