a6c0f7bb9fabb48a6fa5a32cd0597503cb0ded16
Kong Yang <kyang026@gmail.com>
2016-12-20 11:08:14 -0600

    Tried adding a captcha to the contact page

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -3,8 +3,12 @@ class ContactController < ApplicationController
   end
 
   def create
+    if @system_info.captcha
+      should_send = verify_recaptcha
+    end
+
     c = ContactForm.new(name: params[:name], email: params[:email], message: params[:message])
-    if c.valid?
+    if c.valid? and should_send
       c.deliver
       redirect_to contact_index_path, flash: { success: 'Thank you for your inquiry.' }
     else
