73cc02451f42ce319ed8f5c085715e30060cd87b
Chris Dinger <ding0057@umn.edu>
2019-11-04 23:29:20 -0600

    Replace raw SQL migration with add_column
    The oracle adapter handles the type translation for columns declared as
    :boolean in migrations. Raw SQL is not needed here.

--- db/migrate/20161121182621_add_captcha_to_study_finder_system_infos.rb
+++ db/migrate/20161121182621_add_captcha_to_study_finder_system_infos.rb
@@ -1,19 +1,5 @@
 class AddCaptchaToStudyFinderSystemInfos < ActiveRecord::Migration
-  def up
-	  if Rails.env == 'local' or Rails.env == 'test'
-	    # We are using postgres locally, which supports a boolean type.
-	    default = 'FALSE'
-	    type = 'BOOLEAN'
-	  else
-	    # This is an oracle thing.  Oracle doesn't support boolean types. If you are not using oracle, please remove this sillyness and just use visible = true.
-	    default = '0'
-	    type = 'NUMBER(1,0)'
-	  end
-
-	  execute "ALTER TABLE study_finder_system_infos ADD captcha #{type} NOT NULL DEFAULT #{default};"
-	end
-
-	def down
-	  execute "ALTER TABLE study_finder_system_infos DROP COLUMN captcha;"
-	end
+  def change
+    add_column :study_finder_system_infos, :captcha, :boolean, null: false, default: false
+  end
 end
