8160f533c30f97eb745fb6433cd7271bd0fdd133
Chris Dinger <ding0057@umn.edu>
2019-11-11 16:15:49 -0600

    Use docker-compose to run spec on Travis
    Travis provides elasticsearch 5 by default, which causes errors. If we
    want to use 7.4 in this context we'd have to build `before_install`
    scripts to install it. If we're going to have to jump through hoops
    anyway to use a new elasticsearch, we may as well just use
    docker-compose here.
    
    This does make the build run slower (it takes about 6 minutes), but I
    think this is acceptable. It also provide parity with the local
    development and testing environments used by developers.
    
    This commit also removes the empty test directory (this app uses rspec).
    This commit was accidentally rebased into this squashed commit.

--- test/models/condition_group_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ConditionGroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/system_info_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SystemInfoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
