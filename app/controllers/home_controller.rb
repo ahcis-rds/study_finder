f4ee507fa7647b88af57235359ac32f9c7cdbafa
Jacob Johnson <johnsja@umn.edu>
2020-08-26 14:21:32 -0500

    [SF-48] Reimplementing COVID-19 and UMN specific changes to the StudyFinder application.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
+    @featured_category = Group.find_by(group_name: 'COVID-19')
+    
+    #TODO - This should be optimized for studyfinder 2.0
+    @cancer_category = Group.find_by(group_name: 'Cancer')
+    @diabetes_category = Group.find_by(group_name: 'Diabetes & Hormone')
+    @prevention_category = Group.find_by(group_name: 'Prevention')
+    @womens_health_category = Group.find_by(group_name: "Women's Health")
   end
 
   def splash
