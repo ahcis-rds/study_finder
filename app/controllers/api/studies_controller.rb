dfd09e51b082f31e2286d0d127c0393fdfc0cd06
Chris Dinger <ding0057@umn.edu>
2021-01-21 10:07:07 -0600

    Add overall_status to study API

---
+++ app/controllers/api/studies_controller.rb
@@ -0,0 +1,39 @@
+class Api::StudiesController < ApiController
+  def index
+    render json: Trial.all
+  end
+
+  def show
+    @trial = Trial.find_by(system_id: params[:id])
+
+    render json: @trial
+  end
+
+  def update
+    @trial = Trial.find_by(system_id: params[:id])
+
+    if @trial.update(trial_params)
+      head 200
+    else
+      render json: { error: @trial.errors }, status: 400
+    end
+  end
+
+  private
+
+  def trial_params
+    params.permit(
+      :brief_title,
+      :contact_override,
+      :contact_override_first_name,
+      :contact_override_last_name,
+      :irb_number,
+      :overall_status,
+      :pi_id,
+      :pi_name,
+      :recruiting,
+      :simple_description,
+      :visible
+    )
+  end
+end
