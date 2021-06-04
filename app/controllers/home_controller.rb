97497be962780a974108de1ba9ddbf81df4684d3
Chris Dinger <ding0057@umn.edu>
2021-06-03 20:41:13 -0500

    Remove category links on home page
    This mechanism is brittle and causes these links to break when group
    names change.
    
    [SF-119]

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,7 @@
 class HomeController < ApplicationController
   def index
     @featured_category = Group.find_by(group_name: 'COVID-19')
-    
-    #TODO - This should be optimized for studyfinder 2.0
-    @cancer_category = Group.find_by(group_name: 'Cancer')
-    @diabetes_category = Group.find_by(group_name: 'Diabetes & Hormones')
-    @prevention_category = Group.find_by(group_name: 'Prevention')
-    @womens_health_category = Group.find_by(group_name: "Women's Health")
-  	@showcase = ShowcaseItem.where(active: true).order(:sort_order)
+    @showcase = ShowcaseItem.where(active: true).order(:sort_order)
   end
 
   def splash
