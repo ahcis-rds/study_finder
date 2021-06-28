6db62c80114658d947d76c57a91bc5521f67523b
Chris Dinger <ding0057@umn.edu>
2021-06-28 17:01:28 -0500

    Fix ApiKey generation
    `after_initialize` gets called on creates, but also when records are
    fetched from the database. The previous code was causing a key to be
    generated (but not persisted) on every find.

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -2,6 +2,6 @@ class ApiKey < ApplicationRecord
   validates :name, presence: true
 
   after_initialize do |api_key|
-    api_key.token = SecureRandom.base58(32)
+    api_key.token ||= SecureRandom.base58(32)
   end
 end
