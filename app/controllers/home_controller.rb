01f5a2bf45fe6635de2653d597feeb7a0b999f11
Chris Dinger <ding0057@umn.edu>
2021-07-12 20:26:30 -0500

    Link to COVID-19 studies without category
    [SF-139]

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @featured_category = Group.find_by(group_name: 'COVID-19')
     @showcase = ShowcaseItem.where(active: true).order(:sort_order)
   end
 
