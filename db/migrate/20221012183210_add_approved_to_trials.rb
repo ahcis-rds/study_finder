fd00872526c5346dcfa8dbe7df8ee2009446ff68
Brian Hayden <bhayden@umn.edu>
2023-01-18 16:38:39 -0600

    update_all instead of loop

--- db/migrate/20221012183210_add_approved_to_trials.rb
+++ db/migrate/20221012183210_add_approved_to_trials.rb
@@ -2,8 +2,6 @@ class AddApprovedToTrials < ActiveRecord::Migration[5.2]
   def change
     add_column :study_finder_trials, :approved, :boolean, null: false, default: false
 
-    Trial.all.each do |tr|
-      tr.update_attributes(approved: true)
-    end
+    Trial.update_all(approved: true)
   end
 end
