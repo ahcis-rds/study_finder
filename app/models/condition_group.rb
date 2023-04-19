9c468f67c02e109b2f2871724497001fca5115d9
bhayden <bhayden@umn.edu>
2023-04-19 14:05:06 -0500

    Make association optional
    This is an optional association. Mark it as such so updating group conditions doesn't throw a 'trial condition must exist' error.

--- app/models/condition_group.rb
+++ app/models/condition_group.rb
@@ -3,5 +3,5 @@ class ConditionGroup < ApplicationRecord
 
   belongs_to :condition, counter_cache: true
   belongs_to :group
-  belongs_to :trial_condition, foreign_key: :condition_id, primary_key: :condition_id
+  belongs_to :trial_condition, foreign_key: :condition_id, primary_key: :condition_id, optional: true
 end
