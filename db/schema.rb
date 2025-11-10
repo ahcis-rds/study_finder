c1d29a80c2df4d6eb43401931dcb00d1c906caa3
Joe Thor <jfthor@umn.edu>
2025-11-10 09:24:40 -0600

    Bumping schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_25_203753) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_19_110544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
